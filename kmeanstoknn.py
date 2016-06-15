9153ac6a91cdd6cee1413f47aff27f6d7683c982
Axel Schelfhout <axelschelfhout@gmail.com>
2016-06-15 15:22:01 +0200

    Add al

--- kmeanstoknn.py
+++ kmeanstoknn.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import time
 
 from random import randint
 from sklearn import cluster
@@ -9,6 +10,8 @@ from sklearn.cross_validation import train_test_split
 # -- # -- # -- # -- # -- # -- # -- # -- # -- # -- #
 # Data preparation
 
+script_start_time = time.time()  # Timer
+dataprep_start_time = time.time()  # Timer
 file = 'fakenames50k.csv'
 
 # The header is in the first row. By adding header=0 we tell pd.read_csv it is
@@ -31,17 +34,22 @@ df = df.drop(dropable_columns, axis=1)
 print(df.describe())
 
 # The data frames values function will make this in to a Numpy array so we can use it in sklearn.
-pdDf = df
 df = df.values
 
 print(df)
 
+# Shuffle the data.
 np.random.shuffle(df)
 
+dataprep_end_time = time.time()  # Timer
+
+
 # Get a kmeans DF and knn DF
-kmeans_df, knn_df = train_test_split(df, train_size=0.085, random_state=randint(0,len(df)))
+kmeans_df, knn_df = train_test_split(df, train_size=0.08, random_state=randint(0,len(df)))
 
 # Kmeans #
+kmeans_clustercount_start_time = time.time()  # Timer
+# Determine the amount of clusters based on the available data.
 print("How many clusters do we need?")
 calc_clusters = cluster.AffinityPropagation().fit(kmeans_df)
 
@@ -51,20 +59,28 @@ print("We need " + str(len(cluster_centers_indices)))
 # Set the amount of clusters
 n_clusters = len(cluster_centers_indices)
 
-# Start kMeans
+kmeans_start_time = time.time()  # Timer
+# Start kMeans. Set the amount of clusters to the algorithm and fit the data to it.
 k_means = cluster.KMeans(n_clusters=n_clusters, init='k-means++')
 k_means.fit(kmeans_df)
 
-# Get the labels
+# Get the cluster labels from the fitted data.
 labels = k_means.labels_
 
-# Apply the cluster labels to a data frame
+# Apply the cluster labels to a pandas data frame
 kmeans_labeled_data = pd.DataFrame(data=kmeans_df[0:,0:], index=kmeans_df[0:,0], columns=kmeans_df[0,0:])
 
 kmeans_labeled_data['cluster'] = pd.Series(labels, index=kmeans_labeled_data.index)
+
+kmeans_clustercount_end_time = time.time()  # Timer
+kmeans_end_time = time.time()  # Timer
+
 print(kmeans_labeled_data.describe)
 
+# Set the pandas data frame to a numpy array to use it in the kNN algorithm
 labeled_data = kmeans_labeled_data.values
+
+knn_start_time = time.time()  # Timer
 
 # Score kNN model accuracy
 def score_knn_model_by_iterations(data, iterations, train_size, n_neighbors=5):
@@ -92,9 +108,18 @@ knn.fit(kmeans_df, k_means.labels_)
 # Predict over the whole set.
 print(knn.predict(knn_df))
 
-# For each row in the knn dataframe, predict the label
-for row in knn_df:
-    print(row)
-    print(knn.predict([row]))
+knn_end_time = time.time()  # Timer
+script_end_time = time.time()  # Timer
 
-#EOF;
+# For each row in the knn dataframe, predict the label
+# for row in knn_df:
+#     print(row)  # Print the row
+#     print(knn.predict([row]))  # Print the label
+
+print("Data prep time: " + str(dataprep_end_time - dataprep_start_time) + " seconds")
+print("kMeans algorithm: " + str(kmeans_end_time - kmeans_start_time) + " seconds")
+print("kMeans with the cluster determine (AP) : " + str(kmeans_clustercount_end_time - kmeans_clustercount_start_time) + " seconds")
+print("kNN algorithm: " + str(knn_end_time - knn_start_time) + " seconds")
+print("The whole script took : " + str(script_end_time - script_start_time) + " seconds to complete")
+
+# EOF;
